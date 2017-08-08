33f8181bfde559791a38ac8b615eb8fcba118a51
Ratnasetya <ratnasetya209@gmail.com>
2017-08-08 08:54:30 +0700

    init new smartphone view

--- homepage/assets/js/krenova.js
+++ homepage/assets/js/krenova.js
@@ -59,4 +59,12 @@ $(document).ready(function() {
             });
         }
     });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('#menu').fadeIn(500);
+        } else {
+            $('#menu').fadeOut(500);
+        }
+    });
 });
